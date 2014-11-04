9d93ac78a66d6892b606e2a0c7678cd06accc274
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-11-04 15:00:10 -0500

    Making each picture show at its own URL with dynamic routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -23,4 +23,32 @@ class PicturesController < ApplicationController
 			}			
 		]
 	end
+
+	def show
+		@pictures = [
+			{
+				:title 	=> "The old church on the coast of White Sea",
+				:artist => "Sergey Ershov",
+				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg" 
+			},
+			{
+				:title	=> "Sea Power",
+				:artist => "Stephen Scullion",
+				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+			},
+			{
+				:title	=> "Into the Poppies",
+				:artist => "John Wilhelm",
+				:url		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+			},
+			{
+				:title	=> "Giraffe at sunset",
+				:artist => "Andy Rouse",
+				:url		=> "http://i.imgur.com/el0YN3c.jpg"
+			}	
+			]
+			@picture = @pictures[params[:id].to_i]
+			# create @picture variable referencing an id from the pictures array		
+	end
+
 end
