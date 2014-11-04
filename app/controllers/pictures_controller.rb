99f5d8a29a0c9a1ea2ab5a66424c376fe8d054e1
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-11-04 14:31:04 -0500

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,26 @@
 class PicturesController < ApplicationController
 	def index
+		@pictures = [
+			{
+				:title 	=> "The old church on the coast of White Sea",
+				:artist => "Sergey Ershov",
+				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg" 
+			},
+			{
+				:title	=> "Sea Power",
+				:artist => "Stephen Scullion",
+				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+			},
+			{
+				:title	=> "Into the Poppies",
+				:artist => "John Wilhelm",
+				:url		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+			},
+			{
+				:title	=> "Giraffe at sunset",
+				:artist => "Andy Rouse",
+				:url		=> "http://i.imgur.com/el0YN3c.jpg"
+			}			
+		]
 	end
 end
