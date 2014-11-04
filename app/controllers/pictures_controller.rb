4eefa17542bea852d09f6adc9fbdb713bbceeb66
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-11-04 16:41:33 -0500

    moving @pictures in pictures_controller into db

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,54 +1,11 @@
 class PicturesController < ApplicationController
 	def index
-		@pictures = [
-			{
-				:title 	=> "The old church on the coast of White Sea",
-				:artist => "Sergey Ershov",
-				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg" 
-			},
-			{
-				:title	=> "Sea Power",
-				:artist => "Stephen Scullion",
-				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-			},
-			{
-				:title	=> "Into the Poppies",
-				:artist => "John Wilhelm",
-				:url		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-			},
-			{
-				:title	=> "Giraffe at sunset",
-				:artist => "Andy Rouse",
-				:url		=> "http://i.imgur.com/el0YN3c.jpg"
-			}			
-		]
+		@pictures = Picture.all
 	end
 
 	def show
-		@pictures = [
-			{
-				:title 	=> "The old church on the coast of White Sea",
-				:artist => "Sergey Ershov",
-				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg" 
-			},
-			{
-				:title	=> "Sea Power",
-				:artist => "Stephen Scullion",
-				:url 		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-			},
-			{
-				:title	=> "Into the Poppies",
-				:artist => "John Wilhelm",
-				:url		=> "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-			},
-			{
-				:title	=> "Giraffe at sunset",
-				:artist => "Andy Rouse",
-				:url		=> "http://i.imgur.com/el0YN3c.jpg"
-			}	
-			]
-			@picture = @pictures[params[:id].to_i]
-			# create @picture variable referencing an id from the pictures array		
+		@picture = Picture.find(params[:id])
+		# create @picture variable referencing an id from the pictures array		
 	end
 
 	def new
